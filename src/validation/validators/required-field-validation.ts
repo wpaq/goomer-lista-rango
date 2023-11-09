61a4d8ad28e3816bc51f1c92f9866db80e057082
wpaq <wallysonpabloo@gmail.com>
2023-11-09 17:10:10 -0300

    feat: ensure RequiredFieldValidation returns a MissingParamError if validation fails

---
+++ src/validation/validators/required-field-validation.ts
@@ -0,0 +1,13 @@
+import { MissingParamError } from '@/presentation/errors'
+import { type Validation } from '@/presentation/protocols'
+
+export class RequiredFieldValidation implements Validation {
+  constructor (private readonly fieldName: string) {}
+
+  validate (input: any): Error | null {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
+    }
+    return null
+  }
+}
