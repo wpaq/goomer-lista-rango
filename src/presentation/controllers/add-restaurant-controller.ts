098ce667a07f7480ab6f3057ac65d255d2f5bc79
wpaq <wallysonpabloo@gmail.com>
2023-11-09 16:52:43 -0300

    feat: ensure AddRestaurantController returns 500 if AddRestaurant throws

--- src/presentation/controllers/add-restaurant-controller.ts
+++ src/presentation/controllers/add-restaurant-controller.ts
@@ -1,5 +1,5 @@
 import { type Validation, type Controller, type HttpRequest, type HttpResponse } from '@/presentation/protocols'
-import { badRequest, ok } from '@/presentation/helpers'
+import { badRequest, ok, serverError } from '@/presentation/helpers'
 import { type AddRestaurant } from '@/domain/usecases'
 
 export class AddRestaurantController implements Controller {
@@ -9,19 +9,23 @@ export class AddRestaurantController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+
+      const { photo, name, address, openingHours } = httpRequest.body
+      await this.addRestaurant.add({
+        photo,
+        name,
+        address,
+        openingHours
+      })
+
+      return ok('any_data')
+    } catch (error) {
+      return serverError(error)
     }
-
-    const { photo, name, address, openingHours } = httpRequest.body
-    await this.addRestaurant.add({
-      photo,
-      name,
-      address,
-      openingHours
-    })
-
-    return ok('any_data')
   }
 }
