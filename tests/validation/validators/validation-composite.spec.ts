3a79f8f04c9cbf09fdd7472c3c894a989cabd64f
wpaq <wallysonpabloo@gmail.com>
2023-11-09 17:26:48 -0300

    test: ensure Validation Composite return an error if any validation fails

---
+++ tests/validation/validators/validation-composite.spec.ts
@@ -0,0 +1,32 @@
+import { ValidationSpy } from '@/tests/presentation/mocks'
+
+import { ValidationComposite } from '@/validation/validators'
+import { MissingParamError } from '@/presentation/errors'
+
+const field = 'any_field'
+
+type SutTypes = {
+  sut: ValidationComposite
+  validationSpies: ValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpies = [
+    new ValidationSpy(),
+    new ValidationSpy()
+  ]
+  const sut = new ValidationComposite(validationSpies)
+  return {
+    sut,
+    validationSpies
+  }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[1].error = new MissingParamError(field)
+    const error = sut.validate({ [field]: 'any_field' })
+    expect(error).toEqual(validationSpies[1].error)
+  })
+})
