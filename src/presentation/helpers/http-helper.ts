db4fd06648a6d08df142c89ee7425b5fa1ae1137
wpaq <wallysonpabloo@gmail.com>
2023-11-09 10:42:15 -0300

    feat: add badRequest error to http-helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,8 @@ export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
 })
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
