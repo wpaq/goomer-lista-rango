99c1ef6b276898df2dc420785a84a52c61c368bf
wpaq <wallysonpabloo@gmail.com>
2023-11-09 17:41:37 -0300

    feat: add ExpressRouterAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,19 @@
+import { type HttpResponse, type Controller, type HttpRequest } from '@/presentation/protocols'
+
+import { type Request, type Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse: HttpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
