dba5a38295f14ab761fa3495058b1a0cf3a8cb7f
wpaq <wallysonpabloo@gmail.com>
2023-11-09 10:11:13 -0300

    feat: add data source to tests

--- src/infra/db/typeorm/helpers/data-source-test.ts
+++ src/infra/db/typeorm/helpers/data-source-test.ts
@@ -2,13 +2,13 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 
-export const PostgresDataSource = new DataSource({
+export const PostgresDataSourceTest = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT as number | undefined,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST_TEST,
+  port: process.env.DB_PORT_TEST as number | undefined,
+  username: process.env.DB_USERNAME_TEST,
+  password: process.env.DB_PASSWORD_TEST,
+  database: process.env.DB_NAME_TEST,
   synchronize: true,
   logging: false,
   entities: ['src/infra/db/typeorm/entities/*.{ts,js}'],
--- src/infra/db/typeorm/helpers/index.ts
+++ src/infra/db/typeorm/helpers/index.ts
@@ -1,2 +1,3 @@
 export * from './data-source'
+export * from './data-source-test'
 export * from './typeorm-helper'
