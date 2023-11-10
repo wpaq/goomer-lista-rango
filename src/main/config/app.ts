9a1aa374b1195483cd14d8917b9ae53f6f851e32
wpaq <wallysonpabloo@gmail.com>
2023-11-10 09:32:54 -0300

    feat: add setupRoutes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import setupMiddlewares from './middlewares'
+import setupRoutes from './routes'
 import express from 'express'
 
 const app = express()
 setupMiddlewares(app)
+setupRoutes(app)
 export default app
