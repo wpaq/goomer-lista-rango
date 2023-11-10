c547395e3ae2babc61d5a6c985f0dd7122531a7b
wpaq <wallysonpabloo@gmail.com>
2023-11-10 10:27:29 -0300

    test: ensure LogErrorTypeormRepository create an error log on success

---
+++ tests/infra/db/typeorm/log-typeorm-repository.spec.ts
@@ -0,0 +1,34 @@
+import { LogErrorTypeormRepository, TypeormHelper } from '@/infra/db/typeorm'
+import { LogError } from '@/infra/db/typeorm/entities'
+
+import { faker } from '@faker-js/faker'
+
+let logErrorRepository
+
+const makeSut = (): LogErrorTypeormRepository => {
+  return new LogErrorTypeormRepository()
+}
+
+describe('LogErrorTypeorm Repository', () => {
+  beforeAll(async () => {
+    await TypeormHelper.connect('test')
+    logErrorRepository = TypeormHelper.client.getRepository(LogError)
+  })
+
+  beforeEach(async () => {
+    await logErrorRepository.delete({})
+  })
+
+  afterAll(async () => {
+    await TypeormHelper.disconnect()
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    const stack = faker.word.words()
+    await sut.logError(stack)
+
+    const count = await logErrorRepository.countBy({ stack })
+    expect(count).toBe(1)
+  })
+})
