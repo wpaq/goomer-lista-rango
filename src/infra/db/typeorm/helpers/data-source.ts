98832815c38e9db8153387a8cae7373c42874c2b
wpaq <wallysonpabloo@gmail.com>
2023-11-11 18:21:43 -0300

    fix: correct postgres credentials

--- src/infra/db/typeorm/helpers/data-source.ts
+++ src/infra/db/typeorm/helpers/data-source.ts
@@ -4,12 +4,12 @@ import { DataSource } from 'typeorm'
 
 import { LogError, Restaurant } from '../entities'
 
-if (process.env.NODE_ENV === 'development') {
-  process.env.DATABASE_URL = 'postgres://typeorm:password@localhost:5432/goomer-lista-rango'
+if ((process.env.NODE_ENV || '').trim() !== 'development') {
+  process.env.DATABASE_URL = 'postgres://postgres:admin@localhost:5432/goomer-lista-rango'
 }
 
-if (process.env.NODE_ENV === 'test') {
-  process.env.DATABASE_URL = 'postgres://typeorm:password@localhost:5432/goomer-lista-rango-test'
+if ((process.env.NODE_ENV || '').trim() !== 'test') {
+  process.env.DATABASE_URL = 'postgres://postgres:admin@localhost:5432/goomer-lista-rango-test'
 }
 
 export const TypeormDataSource = new DataSource({
