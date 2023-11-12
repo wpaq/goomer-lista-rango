e5ae99a5c92d82c6af0dac15f04f7d8deca6ed30
wpaq <wallysonpabloo@gmail.com>
2023-11-12 18:54:47 -0300

    feat: ensure RestaurantTypeormRepository.loadAll load all restaurants on success

--- src/infra/db/typeorm/repositories/restaurant-typeorm-repository.ts
+++ src/infra/db/typeorm/repositories/restaurant-typeorm-repository.ts
@@ -1,10 +1,10 @@
-import { type AddRestaurantRepository } from '@/data/protocols'
+import { type LoadRestaurantsRepository, type AddRestaurantRepository } from '@/data/protocols'
 import { type RestaurantModel } from '@/domain/models'
 import { type AddRestaurantParams } from '@/domain/usecases'
 import { Restaurant } from '@/infra/db/typeorm/entities'
 import { TypeormHelper } from '@/infra/db/typeorm/helpers'
 
-export class RestaurantTypeormRepository implements AddRestaurantRepository {
+export class RestaurantTypeormRepository implements AddRestaurantRepository, LoadRestaurantsRepository {
   async add (data: AddRestaurantParams): Promise<boolean | RestaurantModel> {
     const { photo, name, address, openingHours } = data
     const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
@@ -15,5 +15,10 @@ export class RestaurantTypeormRepository implements AddRestaurantRepository {
       openingHours
     })
     return await restaurantRepository.save(newRestaurant)
+  }
+
+  async loadAll (): Promise<RestaurantModel[]> {
+    const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
+    return await restaurantRepository.find()
   }
 }
