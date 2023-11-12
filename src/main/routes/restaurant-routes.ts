02330f99b984395298c01b4f8f887bb24753533d
wpaq <wallysonpabloo@gmail.com>
2023-11-12 19:17:40 -0300

    feat: add restaurant get route

--- src/main/routes/restaurant-routes.ts
+++ src/main/routes/restaurant-routes.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddRestaurantController } from '@/main/factories/controllers'
+import { makeAddRestaurantController, makeLoadRestaurantsController } from '@/main/factories/controllers'
 
 import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/restaurant', adaptRoute(makeAddRestaurantController()))
+  router.get('/restaurant', adaptRoute(makeLoadRestaurantsController()))
 }
