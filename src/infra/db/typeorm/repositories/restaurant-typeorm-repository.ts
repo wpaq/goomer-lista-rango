b5d7cd36043a0383072365bbce664733d4950d65
wpaq <wallysonpabloo@gmail.com>
2023-11-14 18:41:20 -0300

    feat: ensure RestaurantTypeormRepository.loadById load an restaurant on success

--- src/infra/db/typeorm/repositories/restaurant-typeorm-repository.ts
+++ src/infra/db/typeorm/repositories/restaurant-typeorm-repository.ts
@@ -1,10 +1,10 @@
-import { type LoadRestaurantsRepository, type AddRestaurantRepository } from '@/data/protocols'
+import { type LoadRestaurantsRepository, type AddRestaurantRepository, type LoadRestaurantByIdRepository } from '@/data/protocols'
 import { type RestaurantModel } from '@/domain/models'
 import { type AddRestaurantParams } from '@/domain/usecases'
 import { Restaurant } from '@/infra/db/typeorm/entities'
 import { TypeormHelper } from '@/infra/db/typeorm/helpers'
 
-export class RestaurantTypeormRepository implements AddRestaurantRepository, LoadRestaurantsRepository {
+export class RestaurantTypeormRepository implements AddRestaurantRepository, LoadRestaurantsRepository, LoadRestaurantByIdRepository {
   async add (data: AddRestaurantParams): Promise<boolean | RestaurantModel> {
     const { photo, name, address, openingHours } = data
     const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
@@ -20,5 +20,12 @@ export class RestaurantTypeormRepository implements AddRestaurantRepository, Loa
   async loadAll (): Promise<RestaurantModel[]> {
     const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
     return await restaurantRepository.find()
+  }
+
+  async loadById (id: string): Promise<RestaurantModel> {
+    const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
+    return await restaurantRepository.findOne({
+      where: { id }
+    }) as RestaurantModel
   }
 }
