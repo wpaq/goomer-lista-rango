96b8a3260a04041c2750f1853898b6545d8850f6
wpaq <wallysonpabloo@gmail.com>
2023-11-18 18:44:32 -0300

    feat: ensure LoadRestaurantById returns 403 if CheckRestaurantById returns false

--- src/presentation/controllers/load-restaurant-by-id-controller.ts
+++ src/presentation/controllers/load-restaurant-by-id-controller.ts
@@ -1,13 +1,17 @@
 import { type CheckRestaurantById } from '@/domain/usecases'
 import { type HttpRequest, type HttpResponse, type Controller } from '@/presentation/protocols'
-import { ok, serverError } from '@/presentation/helpers'
+import { forbidden, ok, serverError } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class LoadRestaurantByIdController implements Controller {
   constructor (private readonly checkRestaurantById: CheckRestaurantById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.checkRestaurantById.checkById(httpRequest.params.restaurantId)
+      const exists = await this.checkRestaurantById.checkById(httpRequest.params.restaurantId)
+      if (!exists) {
+        return forbidden(new InvalidParamError('restaurantId'))
+      }
       return ok('')
     } catch (error) {
       return serverError(error)
