67c7576aa335380d7de1cb34c2b4d3bf9a611e32
wpaq <wallysonpabloo@gmail.com>
2023-11-18 20:28:42 -0300

    feat: add LoadRestaurantByIdController on restaurant routes

--- src/main/routes/restaurant-routes.ts
+++ src/main/routes/restaurant-routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddRestaurantController, makeLoadRestaurantsController } from '@/main/factories/controllers'
+import { makeAddRestaurantController, makeLoadRestaurantsController, makeLoadRestaurantByIdController } from '@/main/factories/controllers'
 
 import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/restaurant', adaptRoute(makeAddRestaurantController()))
   router.get('/restaurant', adaptRoute(makeLoadRestaurantsController()))
+  router.get('/restaurant/:id', adaptRoute(makeLoadRestaurantByIdController()))
 }
