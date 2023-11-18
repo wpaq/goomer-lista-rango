80a771ce34269f424c1322e5709b61e7b3fbd2f4
wpaq <wallysonpabloo@gmail.com>
2023-11-18 18:38:51 -0300

    test: ensure LoadRestaurantById Controller calls CheckRestaurantById with correct id

--- tests/presentation/mocks/mock-restaurant.ts
+++ tests/presentation/mocks/mock-restaurant.ts
@@ -1,6 +1,6 @@
 import { type RestaurantModel } from '@/domain/models'
-import { type LoadRestaurants, type AddRestaurant, type AddRestaurantParams } from '@/domain/usecases'
-import { mockRestaurantsModel } from '@/tests/domain/mocks'
+import { type LoadRestaurants, type AddRestaurant, type AddRestaurantParams, type LoadRestaurantById, type CheckRestaurantById } from '@/domain/usecases'
+import { mockRestaurantModel, mockRestaurantsModel } from '@/tests/domain/mocks'
 
 export class AddRestaurantSpy implements AddRestaurant {
   addRestaurantParams: AddRestaurantParams
@@ -16,6 +16,26 @@ export class LoadRestaurantsSpy implements LoadRestaurants {
   result = mockRestaurantsModel()
 
   async load (): Promise<RestaurantModel[]> {
+    return this.result
+  }
+}
+
+export class CheckRestaurantByIdSpy implements CheckRestaurantById {
+  result: boolean = true
+  id: string
+
+  async checkById (id: string): Promise<boolean> {
+    this.id = id
+    return this.result
+  }
+}
+
+export class LoadRestaurantByIdSpy implements LoadRestaurantById {
+  result = mockRestaurantModel()
+  id: string
+
+  async load (id: string): Promise<RestaurantModel> {
+    this.id = id
     return this.result
   }
 }
