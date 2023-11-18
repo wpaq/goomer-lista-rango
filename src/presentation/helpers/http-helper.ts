4506f6839986b7753403dfffb3a8e113d5de12d5
wpaq <wallysonpabloo@gmail.com>
2023-11-18 18:42:57 -0300

    feat: add forbidden http helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -20,3 +20,8 @@ export const noContent = (): HttpResponse => ({
   statusCode: 204,
   body: null
 })
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
