c31c478b42c73d5cc23b5cf753b216cbf19c5f22
wpaq <wallysonpabloo@gmail.com>
2023-11-20 18:05:15 -0300

    feat: ensure UpdateRestaurant Controller calls CheckRestaurantById with correct id

--- src/presentation/controllers/update-restaurant-controller.ts
+++ src/presentation/controllers/update-restaurant-controller.ts
@@ -1,13 +1,17 @@
 import { type HttpRequest, type HttpResponse, type Controller } from '@/presentation/protocols'
 import { ok, serverError } from '@/presentation/helpers'
-import { type UpdateRestaurant } from '@/domain/usecases'
+import { type CheckRestaurantById, type UpdateRestaurant } from '@/domain/usecases'
 
 export class UpdateRestaurantController implements Controller {
-  constructor (private readonly updateRestaurant: UpdateRestaurant) {}
+  constructor (
+    private readonly checkRestaurantById: CheckRestaurantById,
+    private readonly updateRestaurant: UpdateRestaurant
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const id = httpRequest.params.restaurantId
+      await this.checkRestaurantById.checkById(id)
       const data = httpRequest.body
       const restaurants = await this.updateRestaurant.update(id, data)
       return ok(restaurants)
