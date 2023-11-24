14f0d63249bd8ea169b627e932920024fadbc068
wpaq <wallysonpabloo@gmail.com>
2023-11-24 11:35:26 -0300

    feat: ensure AddProductController call AddProduct with correct values

--- src/presentation/controllers/add-product-controller.ts
+++ src/presentation/controllers/add-product-controller.ts
@@ -1,14 +1,19 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/protocols'
 import { badRequest, noContent } from '@/presentation/helpers'
+import { AddProduct } from '@/domain/usecases'
 
 export class AddProductController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addProduct: AddProduct
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
       return badRequest(error)
     }
+    await this.addProduct.add(httpRequest.body)
     return noContent()
   }
 }
