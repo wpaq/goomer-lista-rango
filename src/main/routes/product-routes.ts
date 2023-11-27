c7b061fc2f1ca8421f4efb493725541a93e66b88
wpaq <wallysonpabloo@gmail.com>
2023-11-27 17:50:57 -0300

    feat: add UpdateProductController route

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddProductController } from '@/main/factories/controllers'
+import { makeAddProductController, makeUpdateProductController } from '@/main/factories/controllers'
 
 import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/product', adaptRoute(makeAddProductController()))
+  router.put('/product/:productId', adaptRoute(makeUpdateProductController()))
 }
