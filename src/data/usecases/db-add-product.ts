b1a0c5c70124e841af2bd296f7651efdaf5bdafd
wpaq <wallysonpabloo@gmail.com>
2023-11-27 10:33:54 -0300

    feat: ensure DbAddProduct return false if CheckRestaurantByIdRepository returns false

--- src/data/usecases/db-add-product.ts
+++ src/data/usecases/db-add-product.ts
@@ -1,11 +1,18 @@
-import { type AddProductRepository } from '@/data/protocols'
+import { CheckRestaurantByIdRepository, type AddProductRepository } from '@/data/protocols'
 import { type ProductModel } from '@/domain/models'
 import { type AddProduct, type AddProductParams } from '@/domain/usecases'
 
 export class DbAddProduct implements AddProduct {
-  constructor (private readonly addProductRepository: AddProductRepository) {}
+  constructor (
+    private readonly checkRestaurantByIdRepository: CheckRestaurantByIdRepository,
+    private readonly addProductRepository: AddProductRepository
+  ) {}
 
   async add (data: AddProductParams): Promise<boolean | ProductModel> {
+    const exists = await this.checkRestaurantByIdRepository.checkById(data.restaurantId)
+    if (!exists) {
+      return false
+    }
     return await this.addProductRepository.add(data)
   }
 }
