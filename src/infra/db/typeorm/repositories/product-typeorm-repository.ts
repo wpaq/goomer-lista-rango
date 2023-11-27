01f35a15c55f00f9cf10241f2290f2582dc7b773
wpaq <wallysonpabloo@gmail.com>
2023-11-27 10:35:30 -0300

    feat: ensure ProductTypeormRepository add an Product on success

--- src/infra/db/typeorm/repositories/product-typeorm-repository.ts
+++ src/infra/db/typeorm/repositories/product-typeorm-repository.ts
@@ -1,13 +1,21 @@
 import { TypeormHelper } from '@/infra/db/typeorm/helpers'
-import { Product } from '@/infra/db/typeorm/entities'
+import { Product, Restaurant } from '@/infra/db/typeorm/entities'
 import { type AddProductRepository } from '@/data/protocols'
-import { type ProductModel } from '@/domain/models'
+import { RestaurantModel, type ProductModel } from '@/domain/models'
 import { type AddProductParams } from '@/domain/usecases'
 
 export class ProductTypeormRepository implements AddProductRepository {
   async add (data: AddProductParams): Promise<boolean | ProductModel> {
+    const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
     const productRepository = TypeormHelper.client.getRepository(Product)
-    const newProduct = productRepository.create(data)
+
+    const restaurant = await restaurantRepository.findOne({ where: { id: data.restaurantId } }) as RestaurantModel
+
+    const newProduct = productRepository.create({
+      ...data,
+      restaurant
+    })
+
     return await productRepository.save(newProduct)
   }
 }
