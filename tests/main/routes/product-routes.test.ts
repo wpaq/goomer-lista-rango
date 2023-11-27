fcadc7673231fbc6c7b96a47b334d9bb3638157e
wpaq <wallysonpabloo@gmail.com>
2023-11-27 10:38:45 -0300

    test: ensure POST /product return 403 if invalid restaurantId is provided

--- tests/main/routes/product-routes.test.ts
+++ tests/main/routes/product-routes.test.ts
@@ -1,20 +1,26 @@
+import { mockAddRestaurantParams } from '@/tests/domain/mocks'
+
 import app from '@/main/config/app'
 import { TypeormHelper } from '@/infra/db/typeorm'
-import { Product } from '@/infra/db/typeorm/entities'
+import { Product, Restaurant } from '@/infra/db/typeorm/entities'
 
-import request from 'supertest'
 import { Repository } from 'typeorm'
+import request from 'supertest'
+import { faker } from '@faker-js/faker'
 
-let ProductRepository: Repository<Product>
+let productRepository: Repository<Product>
+let restaurantRepository: Repository<Restaurant>
 
 describe('Product Routes', () => {
   beforeAll(async () => {
     await TypeormHelper.connect()
-    ProductRepository = TypeormHelper.client.getRepository(Product)
+    productRepository = TypeormHelper.client.getRepository(Product)
+    restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
   })
 
   afterEach(async () => {
-    await ProductRepository.delete({})
+    await productRepository.delete({})
+    await restaurantRepository.delete({})
   })
 
   afterAll(async () => {
@@ -23,25 +29,44 @@ describe('Product Routes', () => {
 
   describe('POST /product', () => {
     test('should return 400 if an required field is not provided', async () => {
+      const restaurant = await restaurantRepository.insert(mockAddRestaurantParams())
+
       await request(app)
         .post('/api/product')
         .send({
           // photo: 'http://www.photo_1.com',
           name: 'Potato Chips',
           price: '10.00',
-          category: 'Food'
+          category: 'Food',
+          restaurantId: restaurant.raw[0].id
         })
         .expect(400)
     })
 
-    test('should return 200 on success', async () => {
+    test('should return 403 if invalid restaurantId is provided', async () => {
       await request(app)
         .post('/api/product')
         .send({
           photo: 'http://www.photo_1.com',
           name: 'Potato Chips',
           price: '10.00',
-          category: 'Food'
+          category: 'Food',
+          restaurantId: faker.string.uuid()
+        })
+        .expect(403)
+    })
+
+    test('should return 200 on success', async () => {
+      const restaurant = await restaurantRepository.insert(mockAddRestaurantParams())
+
+      await request(app)
+        .post('/api/product')
+        .send({
+          photo: 'http://www.photo_1.com',
+          name: 'Potato Chips',
+          price: '10.00',
+          category: 'Food',
+          restaurantId: restaurant.raw[0].id
         })
         .expect(200)
     })
