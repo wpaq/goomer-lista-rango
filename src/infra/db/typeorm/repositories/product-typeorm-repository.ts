e3bd9e8a99472646e237daee923c74cc2baa5f84
wpaq <wallysonpabloo@gmail.com>
2023-11-27 17:56:18 -0300

    feat: ensure ProductTypeormRepository.checkById return true if product exists

--- src/infra/db/typeorm/repositories/product-typeorm-repository.ts
+++ src/infra/db/typeorm/repositories/product-typeorm-repository.ts
@@ -1,10 +1,10 @@
 import { TypeormHelper } from '@/infra/db/typeorm/helpers'
 import { Product, Restaurant } from '@/infra/db/typeorm/entities'
-import { UpdateProductRepository, type AddProductRepository } from '@/data/protocols'
-import { RestaurantModel, type ProductModel } from '@/domain/models'
-import { UpdateProductParams, type AddProductParams } from '@/domain/usecases'
+import { UpdateProductRepository, AddProductRepository, CheckProductByIdRepository } from '@/data/protocols'
+import { type RestaurantModel, type ProductModel } from '@/domain/models'
+import { type UpdateProductParams, type AddProductParams } from '@/domain/usecases'
 
-export class ProductTypeormRepository implements AddProductRepository, UpdateProductRepository {
+export class ProductTypeormRepository implements AddProductRepository, CheckProductByIdRepository, UpdateProductRepository {
   async add (data: AddProductParams): Promise<boolean | ProductModel> {
     const restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
     const productRepository = TypeormHelper.client.getRepository(Product)
@@ -24,5 +24,13 @@ export class ProductTypeormRepository implements AddProductRepository, UpdatePro
     const product = await productRepository.findOne({ where: { id } }) as ProductModel
     Object.assign(product, data)
     return await productRepository.save(product)
+  }
+
+  async checkById (id: string): Promise<boolean> {
+    const productRepository = TypeormHelper.client.getRepository(Product)
+    const product = await productRepository.findOne({
+      where: { id }
+    })
+    return product !== null
   }
 }
