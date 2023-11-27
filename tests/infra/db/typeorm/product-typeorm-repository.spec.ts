cd649abfb94d4afdb6d6d16392654b8d7fcdf202
wpaq <wallysonpabloo@gmail.com>
2023-11-27 10:35:52 -0300

    test: ensure ProductTypeormRepository add an Product on success

--- tests/infra/db/typeorm/product-typeorm-repository.spec.ts
+++ tests/infra/db/typeorm/product-typeorm-repository.spec.ts
@@ -1,11 +1,17 @@
-import { mockAddProductParams } from '@/tests/domain/mocks'
+import { mockAddProductParams, mockAddRestaurantParams } from '@/tests/domain/mocks'
 
 import { ProductTypeormRepository, TypeormHelper } from '@/infra/db/typeorm'
-import { Product } from '@/infra/db/typeorm/entities'
+import { Product, Restaurant } from '@/infra/db/typeorm/entities'
 
 import { Repository } from 'typeorm'
 
 let productRepository: Repository<Product>
+let restaurantRepository: Repository<Restaurant>
+
+const mockRestaurantId = async (): Promise<string> => {
+  const res = await restaurantRepository.insert(mockAddRestaurantParams())
+  return res.raw[0].id
+}
 
 const makeSut = (): ProductTypeormRepository => {
   return new ProductTypeormRepository()
@@ -15,10 +21,12 @@ describe('ProductTypeormRepository', () => {
   beforeAll(async () => {
     await TypeormHelper.connect()
     productRepository = TypeormHelper.client.getRepository(Product)
+    restaurantRepository = TypeormHelper.client.getRepository(Restaurant)
   })
 
   afterEach(async () => {
     await productRepository.delete({})
+    await restaurantRepository.delete({})
   })
 
   afterAll(async () => {
@@ -29,7 +37,7 @@ describe('ProductTypeormRepository', () => {
     test('Should add an Product on success', async () => {
       const sut = makeSut()
       const addProductParams = mockAddProductParams()
-      await sut.add(addProductParams)
+      await sut.add(Object.assign({}, addProductParams, { restaurantId: await mockRestaurantId() }))
       const count = await productRepository.countBy({ name: addProductParams.name })
       expect(count).toBe(1)
     })
