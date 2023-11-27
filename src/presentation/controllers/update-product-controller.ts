02cc83ba5ba1dfab6a792f1c4923459995f46012
wpaq <wallysonpabloo@gmail.com>
2023-11-27 17:38:40 -0300

    feat: ensure UpdateProductController call UpdateProduct with correct values

--- src/presentation/controllers/update-product-controller.ts
+++ src/presentation/controllers/update-product-controller.ts
@@ -1,11 +1,12 @@
 import { type HttpRequest, type HttpResponse, type Controller } from '@/presentation/protocols'
 import { forbidden, noContent, serverError } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
-import { CheckProductById } from '@/domain/usecases'
+import { CheckProductById, UpdateProduct } from '@/domain/usecases'
 
 export class UpdateProductController implements Controller {
   constructor (
-    private readonly checkProductById: CheckProductById
+    private readonly checkProductById: CheckProductById,
+    private readonly updateProduct: UpdateProduct
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -15,6 +16,7 @@ export class UpdateProductController implements Controller {
       if (!exists) {
         return forbidden(new InvalidParamError('id'))
       }
+      await this.updateProduct.update(id, httpRequest.body)
       return noContent()
     } catch (error) {
       return serverError(error)
