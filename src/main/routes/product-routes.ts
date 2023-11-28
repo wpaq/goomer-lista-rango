15acd0c3a4e7fa9c33cf47bf3d479918429bebf5
wpaq <wallysonpabloo@gmail.com>
2023-11-28 18:20:54 -0300

    feat: add product route delete

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddProductController, makeUpdateProductController } from '@/main/factories/controllers'
+import { makeAddProductController, makeUpdateProductController, makeDeleteProductController } from '@/main/factories/controllers'
 
 import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/product', adaptRoute(makeAddProductController()))
   router.put('/product/:productId', adaptRoute(makeUpdateProductController()))
+  router.delete('/product/:productId', adaptRoute(makeDeleteProductController()))
 }
