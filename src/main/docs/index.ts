c64d10f9ec665f41366f1144bbda5035710c1ff0
wpaq <wallysonpabloo@gmail.com>
2023-11-29 10:30:00 -0300

    feat: add swagger index file

---
+++ src/main/docs/index.ts
@@ -0,0 +1,28 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'goomer-lista-rango',
+    description: 'Essa é a documentação da API goomer-lista-rango',
+    version: '1.0.0',
+    contact: {
+      name: 'Wallyson Pablo',
+      email: 'wallysonpabloo@gmail.com',
+      url: 'https://www.linkedin.com/in/wallyson-pablo'
+    },
+    license: {
+      name: 'GPLv3 License',
+      url: 'https://opensource.org/license/gpl-3-0/'
+    }
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Restaurant',
+    description: 'APIs relacionadas ao Restaurant'
+  }, {
+    name: 'Product',
+    description: 'APIs relacionadas ao Product'
+  }]
+}
